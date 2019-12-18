2ea3f3ce4276bdcbb8ce11b038a839d89f7c74ff
Paul Le <paulloukyle@gmail.com>
2019-12-18 12:48:24 +0100

    Toast

--- index.php
+++ index.php
@@ -2,32 +2,41 @@
     session_start();
 ?>
 
-
+<!DOCTYPE html>
 <html>
-    <a href="inscription.php"> INSCRIPTION </a>
-    <br><br>
-    <a href="connexion.php"> CONNEXION </a>
-</html>    
-
-
-<?php
-    
-    if(isset($_SESSION['login']))
-    {
-        echo "Bienvenue ".$_SESSION['login'];
-        echo "<form class='logoutform' method='post' action=''><input class='boutonlogout' type='submit' name='logout' value='Déconnexion'>
-            </form>";
-        if(isset($_POST['logout']))
-        {
-            session_destroy();
-            header('Location: connexion.php');
-        }
-    }
-    else
-    {
-        echo "<br><br>T ki ?";
-    }
-
-    //echo '".$_SESSION['login']."';
-
-?>
+    <head>
+        <title>Accueil</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <section id="menuflexing">
+                    <img id="logoheader" src="images/logoreservationtrue.png">
+                    <ul id="headerflex">
+                        <li><a href="index.php"> INDEX </a></li>
+                        <li><a href="planning.php"> PLANNING </a></li>
+                        <li><a href="reservation.php"> RÉSERVATION </a></li>
+                        <li><a href="profil.php"> PROFIL </a></li>
+                        <?php if(!isset($_SESSION['login'])){?>
+                        <li><a href="inscription.php"> INSCRIPTION </a></li>
+                        <li><a href="connexion.php"> CONNEXION </a></li>
+                        <?php } if(isset($_SESSION['login'])){?>
+                            <form method="post" action="index.php"><input class="boutonlogout" type="submit" name="logout" value="Déconnexion">
+                        </form> 
+                        <?php }if(isset($_POST['logout'])){
+                            session_destroy();
+                            header('Location: connexion.php');
+                            }
+                        ?>
+                    </ul>
+                </section>
+                <section id="bottomheaderbg">
+                    <?php if(isset($_SESSION['login'])){echo "<span class=\"bienvenue\"> Bienvenue ".$_SESSION['login']."</span>";}else{echo "<span class=\"bienvenue\">T ki ?</span>";}?>
+                </section>
+            </nav>
+        <header>
+        <main>
+        </main>
+    </body>
+</html>
--- inscription.php
+++ inscription.php
@@ -7,7 +7,7 @@
     if(isset($_POST['password']))
     {
         $password1 = $_POST['password'];
-        //$passwordhash = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+        $passwordhash = password_hash($password1, PASSWORD_BCRYPT, array('cost' => 12));
     }
 
     if(isset($_POST['inscription']) == true && $_POST['password'] == $_POST['confirmationpassword'] && isset($_POST['login']) && strlen($_POST['login']) != 0 && isset($_POST['password']) && strlen($_POST['password']) != 0 && isset($_POST['confirmationpassword']) && strlen($_POST['confirmationpassword']) != 0)
@@ -26,8 +26,9 @@
         if($inscriptionimpossible == false)
         {
             $namelog = $_POST['login'];
-            $requete2 = "INSERT INTO utilisateurs (login,password) VALUES ('$namelog','$password1')";
+            $requete2 = "INSERT INTO utilisateurs (login,password) VALUES ('$namelog','$passwordhash')";
             $query2 = mysqli_query($connexion , $requete2);
+            header('Location: index.php');  
         }
     }
 
