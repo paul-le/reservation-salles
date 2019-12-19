c3f1a20aea1a8f2aad8d13bf071e77f1069a045d
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2019-12-19 11:12:18 +0100

    Stylisation + ajout d'image

--- connexion.php
+++ connexion.php
@@ -5,15 +5,21 @@
 
     if(isset($_POST['login']) && isset($_POST['password']))
     {
+
         $log = $_POST['login'];
         //$pwd = $_POST['password'];
         $connexion = mysqli_connect("localhost", "root","","reservationsalles");
         $requete = "SELECT count(*) as toast FROM utilisateurs WHERE login = \"$log\"";
         $query = mysqli_query($connexion, $requete);
+        $requetehash = "SELECT password FROM utilisateurs WHERE login = \"$log\"";
+        $query2 = mysqli_query($connexion,$requetehash);
         $resultat = mysqli_fetch_array($query);
         $count = $resultat['toast'];
+        $resultat2 = mysqli_fetch_array($query2);
+        
+        echo $count;
 
-        if($count > 0)
+        if($count > 0 && password_verify($_POST['password'],$resultat2['password']))
         {
             $_SESSION['login'] = $log;
             header('Location: index.php');
--- index.php
+++ index.php
@@ -2,32 +2,120 @@
     session_start();
 ?>
 
-
+<!DOCTYPE html>
 <html>
-    <a href="inscription.php"> INSCRIPTION </a>
-    <br><br>
-    <a href="connexion.php"> CONNEXION </a>
-</html>    
-
-
-<?php
-    
-    if(isset($_SESSION['login']))
-    {
-        echo "Bienvenue ".$_SESSION['login'];
-        echo "<form class='logoutform' method='post' action=''><input class='boutonlogout' type='submit' name='logout' value='Déconnexion'>
-            </form>";
-        if(isset($_POST['logout']))
-        {
-            session_destroy();
-            header('Location: connexion.php');
-        }
-    }
-    else
-    {
-        echo "<br><br>T ki ?";
-    }
-
-    //echo '".$_SESSION['login']."';
-
-?>
+    <head>
+        <title>Accueil</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <section id="menuflexing">
+                    <img id="logoheader" src="images/logoreservationtrue.png">
+                    <ul id="headerflex">
+                        <li class="liheader"><a href="index.php"> INDEX </a></li>
+                        <li class="liheader"><a href="planning.php"> PLANNING </a></li>
+                        <li class="liheader"><a href="reservation.php"> RÉSERVATION </a></li>
+                        <?php if(!isset($_SESSION['login'])){?>
+                        <li class="liheader"><a href="inscription.php"> INSCRIPTION </a></li>
+                        <li class="liheader"><a href="connexion.php"> CONNEXION </a></li>
+                        <?php } if(isset($_SESSION['login'])){?>
+                        <li class="liheader"><a href="profil.php"> PROFIL </a></li>             <li class="liheader"><a href="logout.php"><img id="imglogout" src="images/deconnexion.png"></a></li>
+                        <?php }?>   
+                    </ul>
+                </section>
+                <section id="bottomheaderbg">
+                    <section>
+                    <?php if(isset($_SESSION['login'])){echo "<span class=\"bienvenue\"> Bienvenue ".$_SESSION['login']."</span>";}else{echo "<span class=\"bienvenue\">T ki ?</span>";}?>
+                    </section>
+                </section>
+            </nav>
+        <header>
+        <main id="mainindex">
+            <section class="sectionstyle">
+                <article class="articleimg">
+                    <img src="images/tomatemozza.jpg">
+                </article>
+                <article class="articlemain">
+                    <ul class="articleli1">
+                        <li>Réservation de salles pour dîner</li>
+                        <li>Plusieurs tables disponible</li>
+                        <li>Boisson offerte si plus de 15 personnes</li>
+                    </ul>
+                </article>
+            </section>
+            <section class="sectionstyle">
+                <article class="articleimg">
+                    <img src="images/restausalle2.jpg">
+                </article>  
+                <article>
+                    <ul class="articleli1">
+                        <li>Réservation de salles pour dîner</li>
+                        <li>Plusieurs tables disponible</li>
+                        <li>Boisson offerte si plus de 15 personnes</li>
+                    </ul>
+                </article>
+            </section>
+            <section class="sectionstyle">
+                <article class="articleimg">
+                    <img src="images/birthdaycake.jpg">
+                </article>  
+                <article>
+                    <ul class="articleli1">
+                        <li>Réservation de salles pour dîner</li>
+                        <li>Plusieurs tables disponible</li>
+                        <li>Boisson offerte si plus de 15 personnes</li>
+                    </ul>
+                </article>
+            </section>
+            <section class="sectionstyle">
+                <article class="articleimg">
+                    <img src="images/wedding.jpg">
+                </article>  
+                <article>
+                    <ul class="articleli1">
+                        <li>Réservation de salles pour dîner</li>
+                        <li>Plusieurs tables disponible</li>
+                        <li>Boisson offerte si plus de 15 personnes</li>
+                    </ul>
+                </article>
+            </section>
+        </main>
+        <footer>
+            <section>
+                <article id="footerimglogo">
+                    <img src="images/logoreservationtrue.png">
+                    <span id="titrefooterlogo">Réservation</span>
+                </article>
+            </section>
+            <section>
+                <article>
+                    <ul>
+                        <li class="lifooter"><a href="index.php"> INDEX </a></li>
+                        <li class="lifooter"><a href="planning.php"> PLANNING </a></li>
+                        <li class="lifooter"><a href="reservation.php"> RÉSERVATION </a></li>
+                        
+                        <?php if(!isset($_SESSION['login'])){?>
+                        <li class="lifooter"><a href="inscription.php"> INSCRIPTION </a></li>
+                        <li class="lifooter"><a href="connexion.php"> CONNEXION </a></li>
+                        <?php } if (isset($_SESSION['login'])){ ?>
+                        <li class="lifooter"><a href="profil.php"> PROFIL </a></li> <?php }?>
+                    </ul>
+                </article>
+            </section>
+            <section id="socialfootersection">
+                <article id="socialfooter">
+                    <img src="images/twitter.png">
+                    <img src="images/facebook.png">
+                    <img src="images/insta.png">
+                </article>
+                <article id="socialfooter2">
+                    <img src="images/twitch.png">
+                    <img src="images/discord.png">
+                    <img src="images/youtube.png">
+                </article>
+            </section>
+        </footer>
+    </body>
+</html>
