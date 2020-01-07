9d9c7d32b90cbc327ba529b27a38c2acbe40e245
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-01-07 10:44:34 +0100

    Accueil

--- index.php
+++ index.php
@@ -9,29 +9,7 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <header>
-            <nav>
-                <section id="menuflexing">
-                    <img id="logoheader" src="images/logoreservationtrue.png">
-                    <ul id="headerflex">
-                        <li class="liheader"><a href="index.php"> INDEX </a></li>
-                        <li class="liheader"><a href="planning.php"> PLANNING </a></li>
-                        <li class="liheader"><a href="reservation.php"> RÉSERVATION </a></li>
-                        <?php if(!isset($_SESSION['login'])){?>
-                        <li class="liheader"><a href="inscription.php"> INSCRIPTION </a></li>
-                        <li class="liheader"><a href="connexion.php"> CONNEXION </a></li>
-                        <?php } if(isset($_SESSION['login'])){?>
-                        <li class="liheader"><a href="profil.php"> PROFIL </a></li>             <li class="liheader"><a href="logout.php"><img id="imglogout" src="images/deconnexion.png"></a></li>
-                        <?php }?>   
-                    </ul>
-                </section>
-                <section id="bottomheaderbg">
-                    <section>
-                    <?php if(isset($_SESSION['login'])){echo "<span class=\"bienvenue\"> Bienvenue ".$_SESSION['login']."</span>";}else{echo "<span class=\"bienvenue\">T ki ?</span>";}?>
-                    </section>
-                </section>
-            </nav>
-        <header>
+        <?php include("header.php")?>
         <main id="mainindex">
             <section class="sectionstyle">
                 <article class="articleimg">
@@ -51,7 +29,7 @@
                 </article>  
                 <article>
                     <ul class="articleli1">
-                        <li>Réservation de salles pour dîner</li>
+                        <li>Réservation de salles pour quelconque raison</li>
                         <li>Plusieurs tables disponible</li>
                         <li>Boisson offerte si plus de 15 personnes</li>
                     </ul>
@@ -63,7 +41,7 @@
                 </article>  
                 <article>
                     <ul class="articleli1">
-                        <li>Réservation de salles pour dîner</li>
+                        <li>Réservation de salles pour anniversaire</li>
                         <li>Plusieurs tables disponible</li>
                         <li>Boisson offerte si plus de 15 personnes</li>
                     </ul>
@@ -75,47 +53,14 @@
                 </article>  
                 <article>
                     <ul class="articleli1">
-                        <li>Réservation de salles pour dîner</li>
-                        <li>Plusieurs tables disponible</li>
-                        <li>Boisson offerte si plus de 15 personnes</li>
+                        <li>Réservation de salles pour mariages</li>
+                        <li>Plusieurs salles disponibles</li>
+                        <li>Matériel musical à disposition</li>
                     </ul>
                 </article>
             </section>
         </main>
-        <footer>
-            <section>
-                <article id="footerimglogo">
-                    <img src="images/logoreservationtrue.png">
-                    <span id="titrefooterlogo">Réservation</span>
-                </article>
-            </section>
-            <section>
-                <article>
-                    <ul>
-                        <li class="lifooter"><a href="index.php"> INDEX </a></li>
-                        <li class="lifooter"><a href="planning.php"> PLANNING </a></li>
-                        <li class="lifooter"><a href="reservation.php"> RÉSERVATION </a></li>
-                        
-                        <?php if(!isset($_SESSION['login'])){?>
-                        <li class="lifooter"><a href="inscription.php"> INSCRIPTION </a></li>
-                        <li class="lifooter"><a href="connexion.php"> CONNEXION </a></li>
-                        <?php } if (isset($_SESSION['login'])){ ?>
-                        <li class="lifooter"><a href="profil.php"> PROFIL </a></li> <?php }?>
-                    </ul>
-                </article>
-            </section>
-            <section id="socialfootersection">
-                <article id="socialfooter">
-                    <img src="images/twitter.png">
-                    <img src="images/facebook.png">
-                    <img src="images/insta.png">
-                </article>
-                <article id="socialfooter2">
-                    <img src="images/twitch.png">
-                    <img src="images/discord.png">
-                    <img src="images/youtube.png">
-                </article>
-            </section>
-        </footer>
+            <?php include("footer.php")?>
     </body>
 </html>
+
