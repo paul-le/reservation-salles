d323db47e98ce94302b618fd735ff0c4a5d1ea47
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-01-15 04:10:43 +0100

    Version Final (Ajout MDP HASH)

--- profil.php
+++ profil.php
@@ -15,20 +15,70 @@ $resultat = mysqli_fetch_assoc($query);
         <body>
         <?php include("header.php") ?>
         <main id="mainprofil">
-        <?php if (isset($_SESSION['login'])){ ?>
-                <section id="sectionformprofil">
-                    <form action="" method="post" id="profilform">
-                    <h1 id="h1profil">PROFIL</h1>
-                        <label> Votre Login </label><br>
-                            <input type="text" name="login" value="<?php echo $resultat['login']; ?>"><br>
-                        <label> Votre mot de passe </label><br>
-                            <input type="password" name="password"><br>
-                        <label> Confirmez votre mot de passe </label><br>
-                            <input type="password" name="passwordcon"><br>
-                        <input name="ID" type="hidden" value=<?php echo $resultat['id']; ?>>
-                        <br><br>
-                <section>
-                    <?php } ?>
+            <?php if (isset($_SESSION['login'])){ ?>
+                    <section id="sectionformprofil">
+                        <form action="" method="post" id="profilform">
+                        <h1 id="h1profil">PROFIL</h1>
+                            <label> Votre Login </label><br>
+                                <input type="text" name="login" value="<?php echo $resultat['login']; ?>"><br>
+
+                            <label> Votre mot de passe </label><br>
+                                <input type="password" name="password"><br>
+
+                            <label> Confirmez votre mot de passe </label><br>
+                                <input type="password" name="passwordcon"><br>
+                            
+                            <input type="submit" value="Modifier" name="modifier" /><br>
+                    <section>
+            <?php } ?>
+            <?php 
+
+                if(isset($_POST['modifier']))
+                {
+                    $connexion = mysqli_connect("localhost", "root","", "reservationsalles");
+                        $login = $_POST['login'] ;                   
+                        $requete3 = "SELECT login FROM utilisateurs WHERE login = '$login'";         
+                        $query3 = mysqli_query($connexion, $requete3);         
+                        $resultat3 = mysqli_fetch_all($query3);             
+                        if (!empty($resultat3))             
+                        {                 
+                            echo "Ce Login est déjà prit";             
+                        }
+                        elseif ($_POST['password'] != $_POST['passwordcon']) 
+                        {
+                            echo "Les Mots de passes ne correspondent pas";
+                        }          
+                        else
+                        {
+                            if($_POST['login'] != $resultat['login'])
+
+                            {
+                                
+                               if(isset($_POST["login"]))
+                                {
+                                    $login=$_POST["login"];
+                                }
+                               $connexion = mysqli_connect("localhost","root","","reservationsalles");
+                               $upLog = "UPDATE utilisateurs SET login = \"$login\" WHERE utilisateurs.login='".$resultat['login']."'";
+                               $result = mysqli_query($connexion, $upLog);
+                               echo $upLog;
+
+                            }
+                            if($_POST['password'] != $resultat['password'])
+                            {
+                               $password1 = $_POST['password'];
+                               $passwordhash = password_hash($password1, PASSWORD_BCRYPT, array('cost' => 12));
+                               $connexion = mysqli_connect("localhost","root","","reservationsalles");
+                               $upPass = "UPDATE utilisateurs SET password = \"$passwordhash\" WHERE utilisateurs.password='".$resultat['password']."'";
+                               $result = mysqli_query($connexion, $upPass);
+                               echo $upPass;
+
+                            }
+                        }
+                }
+            ?>
         </main>
         <?php include("footer.php")?>
     </html>
+
+    
